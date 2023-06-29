2dc70c84557d6a8e06738071b8ce22b5bbc30cdc
Nathan Wright <natiouswright@gmail.com>
2023-06-29 18:45:00 -0500

    Formatted code with prettier and eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
-import { useState } from 'react'
+import {useState} from 'react';
 
 function App() {
-  const [newItem, setNewItem] = useState('')
-  const [todos, setTodos] = useState([])
+  const [newItem, setNewItem] = useState('');
+  const [todos, setTodos] = useState([]);
 
-  function handleSubmit () {
-    e.preventDefault()
+  function handleSubmit() {
+    e.preventDefault();
 
     setTodos((currentTodos) => {
       return [
         ...currentTodos,
-        { id: crypto.randomUUID(), title: newItem, completed: false }
-      ]
-    })
+        {id: crypto.randomUUID(), title: newItem, completed: false},
+      ];
+    });
   }
 
   return (
     <>
-    <form className="new-item-form" onSubmit={handleSubmit}>
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input
-          value={newItem}
-          onChange={e => setNewItem(e.target.value)}
-          type="text"
-          id="item"
-        />
-      </div>
-      <button className="btn">Add</button>
-    </form>
-    <h1 className="header">Todo List</h1>
-    <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox" name="" id="" />
-          Item 1
-        </label>
-        <button>Delete</button>
-      </li>
-    </ul>
+      <form
+        className='new-item-form'
+        onSubmit={handleSubmit}
+      >
+        <div className='form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type='text'
+            id='item'
+          />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h1 className='header'>Todo List</h1>
+      <ul className='list'>
+        <li>
+          <label>
+            <input
+              type='checkbox'
+              name=''
+              id=''
+            />
+            Item 1
+          </label>
+          <button>Delete</button>
+        </li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
